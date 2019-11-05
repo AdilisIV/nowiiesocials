396a20c5c3a4460bafe3e65a066718408061dfde
Ilia Fedorov <fedoroff.ilia@gmail.com>
2019-11-05 08:00:45 +0500

    Initial Commit

---
+++ nowiiesocialsTests/nowiiesocialsTests.swift
@@ -0,0 +1,34 @@
+//
+//  nowiiesocialsTests.swift
+//  nowiiesocialsTests
+//
+//  Created by user on 05.11.2019.
+//  Copyright © 2019 Ski, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import nowiiesocials
+
+class nowiiesocialsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
